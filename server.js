864250834c396353deb21a692492e1cbad0a2718
ebrah <jameebrave@gmail.com>
2019-04-12 01:28:59 +0300

    Mongodb setup

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config(); //instead of .load()
+}
+
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
@@ -7,6 +12,11 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.set(expressLayouts)
 app.use(express.static('public'))
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser : true })
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('Connected to Mongoose.'))
 
 app.use('/', indexRouter);
 
